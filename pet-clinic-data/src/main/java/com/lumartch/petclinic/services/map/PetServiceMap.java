86662aef2c0cf8fe545325ed40656b13a4064bb3
Lumart Chryssomallis <lumartch@gmail.com>
2022-01-21 15:43:57 -0600

    Impl Map base service. Closes #12

---
+++ pet-clinic-data/src/main/java/com/lumartch/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.lumartch.petclinic.services.map;
+
+import com.lumartch.petclinic.model.Pet;
+import com.lumartch.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public abstract class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
