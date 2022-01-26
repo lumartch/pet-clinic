50a6051a91d14541b91fd0db47ab3fc3c404c137
Lumart Chryssomallis <lumartch@gmail.com>
2022-01-26 16:59:58 -0600

    Refactored Map Impl to Abstract ID generation. Closes #24

--- pet-clinic-web/src/main/java/com/lumartch/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/lumartch/petclinic/bootstrap/DataLoader.java
@@ -23,18 +23,14 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner ow_1 = new Owner("Lumart","Chryssomallis");
-        ow_1.setId(1L);
         ownerServices.save(ow_1);
         Owner ow_2 = new Owner("Luis","Martínez");
-        ow_2.setId(2L);
         ownerServices.save(ow_2);
         System.out.println("Loading Owners... ");
 
         Vet vet_1 = new Vet("Sam", "Samson");
-        vet_1.setId(1L);
         vetService.save(vet_1);
         Vet vet_2 = new Vet("Peter", "Parker");
-        vet_2.setId(2L);
         vetService.save(vet_2);
         System.out.println("Loading Vets... ");
 
